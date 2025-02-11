1bbb504db2b8997851d911176d92e7b5238bd452
Daniel Macharia <ndungudanny444@gmail.com>
2025-02-11 13:47:12 +0300

    add .vercel.app to allowed_hosts in settings.py

--- archive/settings.py
+++ archive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lk4lxo&=4hub7#)mhm7k-_h2&leibmrlyykccr^1k%2hr85)t7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
