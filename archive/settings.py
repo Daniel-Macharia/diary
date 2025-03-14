7cc4257431b6ad133c48376bdbe8608682bbada3
Daniel-Macharia <140121360+Daniel-Macharia@users.noreply.github.com>
2025-03-14 15:46:13 +0300

    Update settings.py
    add wild character to vercel's allowed server name

--- archive/settings.py
+++ archive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lk4lxo&=4hub7#)mhm7k-_h2&leibmrlyykccr^1k%2hr85)t7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = false
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '*.vercel.app']
 
 
 # Application definition
